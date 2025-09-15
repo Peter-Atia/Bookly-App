01cf1eda955317e949d5954fa6b03c4e1651a789
Peter-Atia <potrosatia1@gmail.com>
2025-09-15 18:27:43 +0300

    refactor code to fit any widget to be animated

---
+++ lib/features/splash/presentation/views/widgets/custom_sliding_widget.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class CustomSlidingWidget extends StatefulWidget {
+
+  const CustomSlidingWidget({super.key, required this.begin, required this.end, required this.widget});
+  final Offset begin ;
+  final Offset end ;
+  final Widget widget;
+  @override
+  State<CustomSlidingWidget> createState() => _CustomSlidingWidgetState();
+}
+
+class _CustomSlidingWidgetState extends State<CustomSlidingWidget> with SingleTickerProviderStateMixin{
+
+
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1),
+    );
+    slidingAnimation = Tween<Offset>(
+      begin: widget.begin,
+      end: widget.end,
+    ).animate(animationController);
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: slidingAnimation,
+      builder: (context, _) {
+        return SlideTransition(
+          position: slidingAnimation,
+          child: widget.widget,
+        );
+      },
+    );
+  }
+}
--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,37 +1,9 @@
 import 'package:bookly_app/core/utils/assets.dart';
-import 'package:bookly_app/features/splash/presentation/views/widgets/sliding_text.dart';
+import 'package:bookly_app/features/splash/presentation/views/widgets/custom_sliding_widget.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatefulWidget {
+class SplashViewBody extends StatelessWidget {
   const SplashViewBody({super.key});
-
-  @override
-  State<SplashViewBody> createState() => _SplashViewBodyState();
-}
-
-class _SplashViewBodyState extends State<SplashViewBody>
-    with SingleTickerProviderStateMixin {
-  late AnimationController animationController;
-  late Animation <Offset> slidingAnimation;
-
-  @override
-  void initState() {
-    super.initState();
-    animationController = AnimationController(
-      vsync: this,
-      duration: Duration(seconds: 1),
-    );
-    slidingAnimation = Tween<Offset>(
-      begin:const Offset(0, 1.5),
-      end:const Offset(0, 0),
-    ).animate(animationController);
-    animationController.forward();
-  }
-  @override
-  void dispose() {
-    animationController.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,8 +11,16 @@ class _SplashViewBodyState extends State<SplashViewBody>
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Image.asset(AssetsData.logo),
-        SlidingText(slidingAnimation :slidingAnimation)
+        CustomSlidingWidget(
+          begin: Offset(0, -.5),
+          end: Offset(0, 0),
+          widget: Image.asset(AssetsData.logo),
+        ),
+        CustomSlidingWidget(
+          begin: Offset(0, 1.5),
+          end: Offset(0, 0),
+          widget: Text("Read free books", textAlign: TextAlign.center),
+        ),
       ],
     );
   }
