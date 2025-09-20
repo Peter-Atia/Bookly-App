02f12b59db21fd7391a96debe2e1ff7435b7eca1
Peter-Atia <potrosatia1@gmail.com>
2025-09-20 14:42:46 +0300

    finish best seller list view

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,7 +1,7 @@
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_appBar.dart';
 import 'package:flutter/material.dart';
-import 'best_seller_list_view_item.dart';
+import 'best_seller_list_view.dart';
 import 'featured_list_view.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -11,16 +11,22 @@ class HomeViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(left: 32),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          CustomAppbar(),
-          FeaturedBooksListView(),
-          Padding(
-            padding: const EdgeInsets.only( top: 20,bottom: 20),
-            child: Text("Best Seller", style: Styles.textStyle18),
+      child: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CustomAppbar(),
+                FeaturedBooksListView(),
+                Padding(
+                  padding: const EdgeInsets.only(top: 20, bottom: 20),
+                  child: Text("Best Seller", style: Styles.textStyle18),
+                ),
+              ],
+            ),
           ),
-          BestSellerItem(),
+          SliverFillRemaining(child: BestSellerListView()),
         ],
       ),
     );
