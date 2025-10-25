c0f64048303df1dc2c18b49d6792b90406682542
Peter-Atia <potrosatia1@gmail.com>
2025-10-25 14:15:53 +0300

    add model and dartz to handel errors early

---
+++ lib/features/home/data/models/book_model/book_model.dart
@@ -0,0 +1,31 @@
+/*
+class BookModel {
+  final String? kind;
+  final String? id;
+  final String? etag;
+  final String? selfLink;
+  final String? volumeInfo;
+  final String? saleInfo;
+  final String? accessInfo;
+  final String? searchInfo;
+
+  const BookModel({
+    this.kind,
+    this.id,
+    this.etag,
+    this.selfLink,
+    this.volumeInfo,
+    this.saleInfo,
+    this.accessInfo,
+    this.searchInfo,
+  });
+  factory BookModel.fromJson(json)=>
+    BookModel(
+      kind: json["kind"]as String,
+      id: json["id"]as String,
+      etag: json["etag"]as String,
+      selfLink: json["selfLink"]as String,
+    );
+
+}
+*/
